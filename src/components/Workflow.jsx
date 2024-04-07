e7be6f76219fa8d25623d6ce6a5a1c30c16ddc88
Mehmet <mehmetimdat.py@gmail.com>
2024-04-07 16:06:39 +0300

    first commit

---
+++ src/components/Workflow.jsx
@@ -0,0 +1,12 @@
+import { checkCirckle2 } from 'lucide-react';
+import codeImg from '../assets/code.jpg';
+
+const Workflow = () => {
+  return (
+    <div className='mt-20'>
+        <h2></h2>
+    </div>
+  );
+};
+
+export default Workflow;
